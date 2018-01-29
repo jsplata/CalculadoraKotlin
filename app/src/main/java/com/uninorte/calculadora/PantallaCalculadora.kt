3c9da7d689622ef2b824cb64504de8270ef2402b
jsplata <jsplata@uninorte.edu.co>
2018-01-29 17:31:15 -0500

    eee
    e

---
+++ app/src/main/java/com/uninorte/calculadora/PantallaCalculadora.kt
@@ -0,0 +1,18 @@
+package com.uninorte.calculadora
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PantallaCalculadora : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_calculadora)
+
+
+    }
+
+
+
+
+}
